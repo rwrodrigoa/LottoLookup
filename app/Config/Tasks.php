29aa7fc7ed4b756ae690fc984339eb2613e96058
Rodrigo Weiss <rwrodrigoa@gmail.com>
2024-02-17 23:41:07 -0300

    Adicionado arquivo de configuração de tarefas

---
+++ app/Config/Tasks.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter Tasks.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Config;
+
+use CodeIgniter\Tasks\Config\Tasks as BaseTasks;
+use CodeIgniter\Tasks\Scheduler;
+
+class Tasks extends BaseTasks
+{
+    /**
+     * --------------------------------------------------------------------------
+     * Should performance metrics be logged
+     * --------------------------------------------------------------------------
+     *
+     * If true, will log the time it takes for each task to run.
+     * Requires the settings table to have been created previously.
+     */
+    public bool $logPerformance = true;
+
+    /**
+     * --------------------------------------------------------------------------
+     * Maximum performance logs
+     * --------------------------------------------------------------------------
+     *
+     * The maximum number of logs that should be saved per Task.
+     * Lower numbers reduced the amount of database required to
+     * store the logs.
+     */
+    public int $maxLogsPerTask = 500;
+
+    /**
+     * Register any tasks within this method for the application.
+     * Called by the TaskRunner.
+     */
+    public function init(Scheduler $schedule)
+    {
+        $schedule->command('draws:update')->hours([0,12,21,22]);
+    }
+}
