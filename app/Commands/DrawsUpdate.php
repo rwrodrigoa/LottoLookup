a17d7abc9033446fa887fd0ad496b7e5aa84acad
Rodrigo Weiss <rwrodrigoa@gmail.com>
2024-02-17 21:28:12 -0300

    Adiciona classe DrawsUpdate e ApiHelper

---
+++ app/Commands/DrawsUpdate.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Commands;
+
+use App\Helpers\ApiHelper;
+use App\Models\DrawResultModel;
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use DateTime;
+
+class DrawsUpdate extends BaseCommand
+{
+    /**
+     * The Command's Group
+     *
+     * @var string
+     */
+    protected $group = 'Api';
+
+    /**
+     * The Command's Name
+     *
+     * @var string
+     */
+    protected $name = 'draws:update';
+
+    /**
+     * The Command's Description
+     *
+     * @var string
+     */
+    protected $description = 'Atualiza os dados dos jogos';
+
+    /**
+     * The Command's Usage
+     *
+     * @var string
+     */
+    protected $usage = 'draws:update';
+
+
+    protected $urls = [
+        "maismilionaria",
+        "megasena",
+        "lotofacil",
+        "quina",
+        "lotomania",
+        "timemania",
+        "duplasena",
+        "federal",
+        "diadesorte",
+        "supersete"
+    ];
+
+    /**
+     * Actually execute a command.
+     *
+     * @param array $params
+     */
+    public function run(array $params)
+    {
+        $apiHelper = new ApiHelper();
+        $drawResultModel = new DrawResultModel();
+
+        foreach ($this->urls as $url) {
+            $fullUrl = 'https://servicebus2.caixa.gov.br/portaldeloterias/api/' . $url;
+
+            try {
+                $data = $apiHelper->fetchDataFromApi($fullUrl);
+
+                $drawData = [
+                    'draw_type'   => $url,
+                    'draw_number' => $data['numero'],
+                    'draw_result' => json_encode($data),
+                    'draw_date'   => DateTime::createFromFormat('d/m/Y', $data['dataApuracao'])->format('Y-m-d')
+                ];
+
+                $existingRecord = $drawResultModel
+                    ->where('draw_type', $drawData['draw_type'])
+                    ->where('draw_number', $drawData['draw_number'])
+                    ->first();
+
+                if (!$existingRecord) {
+                    $drawResultModel->insert($drawData);
+                    CLI::write('Dados de ' . ucfirst($url) . 'inseridos com sucesso no banco!', 'green');
+                } else {
+                    $drawResultModel->update($existingRecord->id, $drawData);
+                    CLI::write('Dados de ' . ucfirst($url) . 'atualizados com sucesso no banco!', 'green');
+                }
+
+            } catch (\Exception $e) {
+                CLI::error('Erro ao salvar/atualizar os dados de ' . ucfirst($url) . ': ' . $e->getMessage(), 'red');
+            }
+        }
+    }
+}
---
+++ app/Helpers/ApiHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Helpers;
+
+use GuzzleHttp\Client;
+
+class ApiHelper
+{
+
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client(['verify' => false]);
+    }
+ 
+    public function fetchDataFromApi($url)
+    {
+        $response = $this->client->get($url);
+        
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
+            die('Erro ao decodificar JSON: ' . json_last_error_msg());
+        }
+
+        return $data;
+    }
+    
+}
