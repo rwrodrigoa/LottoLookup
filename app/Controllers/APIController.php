48dd0856d7be20bb29bcaf6466f8c572efe421a6
Rodrigo Weiss <rwrodrigoa@gmail.com>
2024-02-17 22:21:25 -0300

    Adiciona lógica para buscar resultados de sorteios em API e retornar em formato JSON

--- app/Controllers/APIController.php
+++ app/Controllers/APIController.php
@@ -3,29 +3,30 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Helpers\ApiHelper;
+use App\Models\DrawResultModel;
 use CodeIgniter\API\ResponseTrait;
 
 class APIController extends BaseController
 {
     use ResponseTrait;
-    
+
     public function index()
     {
-        $data = [
-            'title' => 'LottoLookup, as seguintes opções estão disponíveis:',
-            'options' => [
-                "maismilionaria",
-                "megasena",
-                "lotofacil",
-                "quina",
-                "lotomania",
-                "timemania",
-                "duplasena",
-                "federal",
-                "diadesorte",
-                "supersete"
-            ]
-        ];
+        $apiHelper = new ApiHelper();
+        $drawResultModel = new DrawResultModel();
+        $data = [];
+
+        foreach ($apiHelper->draws as $draw) {
+            $data[$draw] = json_decode(
+                $drawResultModel
+                    ->where('draw_type', $draw)
+                    ->orderBy('draw_date', 'desc')
+                    ->first()
+                    ->draw_result,
+                true
+            );
+        }
 
         return $this->respond($data, 200);
     }
